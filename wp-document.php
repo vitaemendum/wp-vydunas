bdb9fb5828dbb8b5a1dbb7d77f5928c857c360f9
beviltiskas <matasbendikas0@gmail.com>
2023-04-20 19:51:42 +0300

    book and document plugin updated

--- wp-document.php
+++ wp-document.php
@@ -2,7 +2,7 @@
 
 /**
  * Plugin Name: Document 
- * Description: Document pluhin
+ * Description: Document plugin
  * Version:     1.0.0
  * License:     GPLv2
  * Network:     true
@@ -11,7 +11,7 @@
 require_once ABSPATH . '/wp-admin/includes/file.php';
 require_once(ABSPATH . 'wp-admin/includes/image.php');
 
-// Custom post type for documents
+// Registering custom post type for documents
 add_action('init', function () {
   register_post_type('document', [
     'labels' => [
@@ -20,6 +20,11 @@ add_action('init', function () {
     ],
     'public' => false,
     'capability_type' => 'post',
+    'hierarchical'       => false,
+    'menu_position'      => null,
+    'show_in_rest'       => true,
+    'rest_base'          => 'documents',
+    'rest_controller_class' => 'WP_REST_Posts_Controller',
     'supports' => ['title', 'author', 'thumbnail']
   ]);
 });
@@ -85,17 +90,7 @@ add_action('rest_api_init', 'register_document_fields');
 
 add_action('rest_api_init', function () {
   register_rest_route('wp/v2', '/documents', [
-    'methods' => 'POST',
-    'callback' => 'create_document',
-    'permission_callback' => function ($request) {
-      return current_user_can('edit_posts');
-    }
-  ]);
-});
-
-add_action('rest_api_init', function () {
-  register_rest_route('wp/v2', '/documents', [
-    'methods' => 'GET',
+    'methods' => Wp_rest_server::READABLE,
     'callback' => 'get_documents',
     'permission_callback' => function ($request) {
       return current_user_can('edit_posts');
@@ -105,8 +100,25 @@ add_action('rest_api_init', function () {
 
 add_action('rest_api_init', function () {
   register_rest_route('wp/v2', '/documents/(?P<id>\d+)', [
-    'methods' => 'GET',
+    'methods' => WP_REST_Server::READABLE,
     'callback' => 'get_document',
+    'args' => array(
+      'id' => array(
+        'validate_callback' => function ($value) {
+          return is_numeric($value);
+        }
+      ),
+    ),
+    'permission_callback' => function ($request) {
+      return current_user_can('edit_posts');
+    }
+  ]);
+});
+
+add_action('rest_api_init', function () {
+  register_rest_route('wp/v2', '/documents', [
+    'methods' => WP_REST_Server::CREATABLE,
+    'callback' => 'create_document',
     'permission_callback' => function ($request) {
       return current_user_can('edit_posts');
     }
@@ -115,8 +127,15 @@ add_action('rest_api_init', function () {
 
 add_action('rest_api_init', function () {
   register_rest_route('wp/v2', '/documents/(?P<id>\d+)', [
-    'methods' => 'PUT',
+    'methods' => WP_REST_Server::EDITABLE,
     'callback' => 'update_document',
+    'args' => array(
+      'id' => array(
+        'validate_callback' => function ($value) {
+          return is_numeric($value);
+        }
+      ),
+    ),
     'permission_callback' => function ($request) {
       return current_user_can('edit_posts');
     }
@@ -125,149 +144,25 @@ add_action('rest_api_init', function () {
 
 add_action('rest_api_init', function () {
   register_rest_route('wp/v2', '/documents/(?P<id>\d+)', [
-    'methods' => 'DELETE',
+    'methods' => WP_REST_Server::DELETABLE,
     'callback' => 'delete_document',
+    'args' => array(
+      'id' => array(
+        'validate_callback' => function ($value) {
+          return is_numeric($value);
+        }
+      ),
+    ),
     'permission_callback' => function ($request) {
       return current_user_can('delete_posts');
     }
   ]);
 });
 
-function update_document($request)
-{
-  $params = $request->get_params();
-  $id = $params['id'];
-  // Validate input ID
-  if (!$id || !is_numeric($id)) {
-    return new WP_Error('invalid_input', __('Invalid input ID'), ['status' => 400]);
-  }
-  // Validate required fields
-  if (empty($params['title'])) {
-    return new WP_Error('missing_title', __('Title is required.', 'text-domain'), array('status' => 400));
-  }
-  if (empty($params['document_category'])) {
-    return new WP_Error('missing_category', __('Category is required.', 'text-domain'), array('status' => 400));
-  }
-  // Sanitize and validate input data
-  $title = sanitize_text_field($params['title']);
-  $document_category = sanitize_text_field($params['document_category']);
-
-  // Get the existing document post
-  $post = get_post($id);
-  if (!$post) {
-    return new WP_Error('document_not_found', __('Document not found'), ['status' => 404]);
-  }
-
-  // Check if there's a new file upload
-  if (!empty($_FILES['document_file'])) {
-    $file = $_FILES['document_file'];
-    // Check for file upload errors
-    if ($file['error'] !== UPLOAD_ERR_OK) {
-      return new WP_Error('invalid_upload', __('Invalid file upload'), ['status' => 400]);
-    }
-    // Validate file type
-    $allowed_types = ['pdf', 'doc', 'docx', 'xls', 'xlsx'];
-    $file_ext = pathinfo($file['name'], PATHINFO_EXTENSION);
-    if (!in_array(strtolower($file_ext), $allowed_types)) {
-      return new WP_Error('invalid_file_type', __('Invalid file type'), ['status' => 400]);
-    }
-    // Process and validate the file upload
-    $uploaded_file = wp_handle_upload($file, ['test_form' => false]);
-    if ($uploaded_file && !isset($uploaded_file['error'])) {
-      $file_name = basename($file['name']);
-      $file_type = wp_check_filetype($file_name, null);
-
-      // Update the document post
-      $post_data = [
-        'ID' => $id,
-        'post_title' => $title,
-        'meta_input' => [
-          'category' => $document_category,
-          'file_type' => $file_type['ext'],
-        ]
-      ];
-      wp_update_post($post_data);
-
-      // Delete the existing file attachment
-      $attachments = get_attached_media('', $id);
-      if (!empty($attachments)) {
-        foreach ($attachments as $attachment) {
-          wp_delete_attachment($attachment->ID, true);
-        }
-      }
-
-      // Attach the new file to the post
-      $attachment = [
-        'guid' => $uploaded_file['url'],
-        'post_mime_type' => $file_type['type'],
-        'post_title' => $file_name,
-        'post_content' => '',
-        'post_status' => 'inherit',
-        'post_parent' => $id,
-        
-      ];
-      $attachment_id = wp_insert_attachment($attachment, $uploaded_file['file'], $id);
-      if (!is_wp_error($id)) {
-        $attachment_data = wp_generate_attachment_metadata($attachment_id, $uploaded_file['file']);
-        wp_update_attachment_metadata($attachment_id, $attachment_data);
-      } else {
-        // Revert to the old file attachment if there was an error
-        $attachment_id = add_attachment_by_url($post->ID, $post->guid);
-      }
-    }
-  }
-
-  // Get the updated document data
-  $updated_post = get_post($id);
-  // Return the updated document data
-  $response = [
-    'id' => $updated_post->ID,
-    'title' => $updated_post->post_title,
-    'category' => $document_category,
-  ];
-  return $response;
-}
-
-function delete_document($request)
-{
-  $id = $request->get_param('id');
-
-  // Validate input ID
-  if (!$id || !is_numeric($id)) {
-    return new WP_Error('invalid_input', __('Invalid input ID'), ['status' => 400]);
-  }
-
-  $post = get_post($id);
-  if (!$post || $post->post_type !== 'document') {
-    return new WP_Error('not_found', __('Document not found'), ['status' => 404]);
-  }
-
-  // Delete event attachment if it exists
-  $attachment = get_posts([
-    'post_type' => 'attachment',
-    'post_parent' => $id,
-  ]);
-  $attachment_id = $attachment ? $attachment[0]->ID : null;
-  if ($attachment_id) {
-    wp_delete_attachment($attachment_id, true);
-  }
-
-  $result = wp_delete_post($id, true);
-
-  // Check if post is successfully deleted
-  if (!$result || is_wp_error($result)) {
-    return new WP_Error('delete_failed', __('Failed to delete document'), ['status' => 500]);
-  }
-  return [
-    'id' => $id,
-    'message' => __('Document deleted successfully')
-  ];
-}
-
 function get_document($request)
 {
   $params = $request->get_params();
-  $post_id = $params['id']; // Assuming this is the ID returned in the response of the create method
+  $post_id = $params['id'];
 
   $post = get_post($post_id);
   if (!$post) {
@@ -306,7 +201,7 @@ function get_documents($request)
     return new WP_Error('query_failed', __('No documents found'), ['status' => 404]);
   }
 
-  $documents = [];
+  $response = [];
   foreach ($query->posts as $post) {
     $attachments = get_posts([
       'post_type' => 'attachment',
@@ -316,7 +211,7 @@ function get_documents($request)
     $attachment_id = $attachments ? $attachments[0]->ID : null;
     $attachment_url = $attachment_id ? wp_get_attachment_url($attachment_id) : null;
 
-    $documents[] = [
+    $response[] = [
       'id' => $post->ID,
       'title' => $post->post_title,
       'category' => get_post_meta($post->ID, 'category', true),
@@ -326,14 +221,14 @@ function get_documents($request)
     ];
   }
 
-  return $documents;
+  return $response;
 }
 
 function create_document($request)
 {
   $params = $request->get_params();
   // Validate required fields
-  if (empty($params['title']) || empty($params['document_category'])) {
+  if (empty($params['title'])) {
     return new WP_Error('missing_title', __('Title is required.', 'text-domain'), array('status' => 400));
   }
   if (empty($params['document_category'])) {
@@ -401,4 +296,144 @@ function create_document($request)
   } else {
     return new WP_Error('upload_failed', __('Failed to upload document'), ['status' => 500]);
   }
+}
+
+function update_document($request)
+{
+  $params = $request->get_params();
+  $id = $params['id'];
+
+  // Validate required fields
+  if (empty($params['title'])) {
+    return new WP_Error('missing_title', __('Title is required.', 'text-domain'), array('status' => 400));
+  }
+  if (empty($params['document_category'])) {
+    return new WP_Error('missing_category', __('Category is required.', 'text-domain'), array('status' => 400));
+  }
+
+  // Sanitize and validate input data
+  $title = sanitize_text_field($params['title']);
+  $document_category = sanitize_text_field($params['document_category']);
+
+  // Get the existing document post
+  $post = get_post($id);
+  if (!$post) {
+    return new WP_Error('document_not_found', __('Document not found'), ['status' => 404]);
+  }
+
+  // Check if there's a new file upload
+  if (!empty($_FILES['document_file'])) {
+    $file = $_FILES['document_file'];
+    // Check for file upload errors
+    if ($file['error'] !== UPLOAD_ERR_OK) {
+      return new WP_Error('invalid_upload', __('Invalid file upload'), ['status' => 400]);
+    }
+    // Validate file type
+    $allowed_types = ['pdf', 'doc', 'docx', 'xls', 'xlsx'];
+    $file_ext = pathinfo($file['name'], PATHINFO_EXTENSION);
+    if (!in_array(strtolower($file_ext), $allowed_types)) {
+      return new WP_Error('invalid_file_type', __('Invalid file type'), ['status' => 400]);
+    }
+    // Process and validate the file upload
+    $uploaded_file = wp_handle_upload($file, ['test_form' => false]);
+    if ($uploaded_file && !isset($uploaded_file['error'])) {
+      $file_name = basename($file['name']);
+      $file_type = wp_check_filetype($file_name, null);
+
+      // Update the document post
+      $post_data = [
+        'ID' => $id,
+        'post_title' => $title,
+        'meta_input' => [
+          'category' => $document_category,
+          'file_type' => $file_type['ext'],
+        ]
+      ];
+      wp_update_post($post_data);
+
+      // Delete the existing file attachment
+      $attachments = get_attached_media('', $id);
+      if (!empty($attachments)) {
+        foreach ($attachments as $attachment) {
+          wp_delete_attachment($attachment->ID, true);
+        }
+      }
+
+      // Attach the new file to the post
+      $attachment = [
+        'guid' => $uploaded_file['url'],
+        'post_mime_type' => $file_type['type'],
+        'post_title' => $file_name,
+        'post_content' => '',
+        'post_status' => 'inherit',
+        'post_parent' => $id,
+
+      ];
+      $attachment_id = wp_insert_attachment($attachment, $uploaded_file['file'], $id);
+      if (!is_wp_error($id)) {
+        $attachment_data = wp_generate_attachment_metadata($attachment_id, $uploaded_file['file']);
+        wp_update_attachment_metadata($attachment_id, $attachment_data);
+      } else {
+        // Revert to the old file attachment if there was an error
+        $attachment_id = add_attachment_by_url($post->ID, $post->guid);
+      }
+    }
+  } else {
+    // Update the document post with new title and meta data only
+    $post_data = [
+      'message' => 'Document updated successfully.',
+      'data'    => array(
+        'id' => $id,
+        'post_title' => $title,
+        'meta_input' => [
+          'category' => $document_category,
+        ],
+      ),
+    ];
+    wp_update_post($post_data);
+  }
+
+  // Get the updated document data
+  $updated_post = get_post($id);
+  // Return the updated document data
+  $response = [
+    'message' => 'Document updated successfully.',
+    'data'    => array(
+      'id' => $updated_post->ID,
+      'title' => $updated_post->post_title,
+      'category' => $document_category,
+    ),
+  ];
+  return $response;
+}
+
+function delete_document($request)
+{
+  $id = $request->get_param('id');
+
+  $post = get_post($id);
+  if (!$post || $post->post_type !== 'document') {
+    return new WP_Error('not_found', __('Document not found'), ['status' => 404]);
+  }
+
+  // Delete document attachment if it exists
+  $attachment = get_posts([
+    'post_type' => 'attachment',
+    'post_parent' => $id,
+  ]);
+  $attachment_id = $attachment ? $attachment[0]->ID : null;
+  if ($attachment_id) {
+    wp_delete_attachment($attachment_id, true);
+  }
+
+  $result = wp_delete_post($id, true);
+
+  // Check if post is successfully deleted
+  if (!$result || is_wp_error($result)) {
+    return new WP_Error('delete_failed', __('Failed to delete document'), ['status' => 500]);
+  }
+  return [
+    'id' => $id,
+    'message' => __('Document deleted successfully')
+  ];
 }
